500b6257d5624ca3a098c06685fd2754acb7817c
Tobias Raidl <e11717659@student.tuwien.ac.at>
2024-10-07 21:44:23 +0200

    reformated gui layout and node info is now a card

--- app.py
+++ app.py
@@ -8,8 +8,7 @@ from graph_elements import generate_elements
 import pandas as pd
 import dash_bootstrap_components as dbc
 
-# Create the Dash app
-app = dash.Dash(__name__)
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 df = load_data('data/multiplatform_hashed_visuals.csv')
 MIN_THRESHOLD = 100
@@ -33,35 +32,49 @@ LAYOUT_NAMES = [
 app.layout = html.Div([
     dbc.Row([
         dbc.Col(
+            cyto.Cytoscape(
+                id='cytoscape-graph',
+                layout={'name': 'cose'},
+                style={'width': '100%', 'height': '600px', "border-style": "groove"},
+                elements=generate_elements(G, INITIAL_MIN_DEGREE)
+            )
+        ),
+        dbc.Col([
             dcc.Slider(
                 MIN_THRESHOLD, MAX_THRESHOLD, SLIDER_STEPS,
                 value=INITIAL_MIN_DEGREE,
                 id="min-degree-slider",
             ),
-            width=6
-        ),
-        dbc.Col(
-            dbc.Select(
-                id="graph-layout-select",
-                value="cose",
-                options=[
-                    {"label": name.capitalize(), "value": name} 
-                    for name in LAYOUT_NAMES
-                ]
-            ),
-            width=6
-        )
+            dbc.Col([
+                dbc.Label("Layout", html_for="graph-layout-select")
+            ], width=2),
+            dbc.Col([
+                dbc.Select(
+                    id="graph-layout-select",
+                    value="cose",
+                    options=[
+                        {"label": name.capitalize(), "value": name} 
+                        for name in LAYOUT_NAMES
+                    ]
+                ),
+            ], width=2),
+            
+            dbc.Row(
+                dbc.Card(
+                    dbc.CardBody(
+                        id="node-info",
+                        children=[  # Use 'children' to specify the contents properly
+                            html.H4("Node Name", className="card-title"),
+                            html.H6("Party", className="card-subtitle"),
+                            html.P("Node ID", className="card-text"),
+                        ]
+                    )
+                )
+            )
+        ]),
     ]),
-    dbc.Row(cyto.Cytoscape(
-        id='cytoscape-graph',
-        layout={'name': 'cose'},
-        style={'width': '100%', 'height': '600px', "border-style": "groove"},
-        elements=generate_elements(G, INITIAL_MIN_DEGREE)
-    )),
-    dbc.Row(
-        html.Div(id='node-info', style={'margin-top': '20px'})
-    )
 ])
+
 
 @callback(
     Output("cytoscape-graph", "elements"),
@@ -89,10 +102,12 @@ def display_node_info(node_data):
         return "Click on a node to see its information."
     
     # Return the information from the clicked node
-    return f"""
-        Node ID: {node_data['id']} \n
-        Name: {node_data['name']}
-        """
+    card =  [
+                html.H4(f"Name: {node_data['name']}", className="card-title"),
+                html.H6(f"Party: {node_data['party']}", className="card-subtitle"),
+                html.P(f"Node ID: {node_data['id']}", className="card-text"),
+            ]
+    return card
 
 if __name__ == '__main__':
     app.run_server(debug=True)
